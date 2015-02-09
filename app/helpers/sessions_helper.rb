7227b69a24d75ec570861c939cfa42104350f44e
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-02-09 15:30:27 +0000

    adds a logged_in? method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,4 +8,9 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
 end
