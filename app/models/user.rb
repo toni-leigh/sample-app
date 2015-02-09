3241940fb8b42dfeb589564c6841075792135a11
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-02-09 10:28:25 +0000

    add a constraint on the password that it must be six characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,8 @@ class User < ActiveRecord::Base
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
 
+  validates :password,
+    length: { minimum: 6 }
+
   has_secure_password
 end
