6e8d64ba10c8337725e80d5a9d0b301e9d051e4e
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-02-09 15:30:11 +0000

    comments

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,10 @@
 module SessionsHelper
+  # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
   end
