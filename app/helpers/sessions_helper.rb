d0cdc30422c4a4688b3f2efba3ff88b62dbd1c44
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-02-09 16:13:10 +0000

    add logout functionality with tests

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,12 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
