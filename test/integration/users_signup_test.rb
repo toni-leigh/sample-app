9c7d2a18d005a8b86cd0f1a4facbd93f5e82c9f4
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-02-09 14:25:57 +0000

    add more tests for validity of signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
+    this_fail_thrown_errors_count = 4
     assert_no_difference 'User.count' do
       post users_path, user: { name:  "",
                                email: "user@invalid",
@@ -11,8 +12,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
+    assert_select 'div#error_explanation .alert', "The form contains #{this_fail_thrown_errors_count} errors."
     assert_select 'div#error_explanation ul li'
+    assert_select 'div#error_explanation ul li', this_fail_thrown_errors_count
     assert_select 'div.field_with_errors'
+    assert_not flash[:success]
   end
   
   test "valid signup information" do
@@ -24,5 +28,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert flash[:success]
+    assert_select 'div.alert-success', 'Welcome to the Sample App!'
   end
 end
