7a1d67ef1c2faf9f60b7fb314383e70425867110
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-02-10 08:54:27 +0000

    add functionality to remember a user in the database using secure token like a password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
 
   validates :name, 
@@ -17,10 +19,20 @@ class User < ActiveRecord::Base
 
   has_secure_password  
 
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
   end
 end
