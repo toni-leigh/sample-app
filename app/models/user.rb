e69f1127a69afc7a60570babc1444be826c2240d
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-02-10 09:17:34 +0000

    remember users using cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,9 +19,20 @@ class User < ActiveRecord::Base
 
   has_secure_password  
 
+  # Remembers a user in the database for use in persistent sessions
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
   # Returns the hash digest of the given string.
