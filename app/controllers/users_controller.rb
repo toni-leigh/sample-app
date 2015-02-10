f7d8bb551170dcaa55f3276971d563fc4f65d9c0
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-02-10 11:14:36 +0000

    redirects any logged out attempt to look at user edit or update pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -33,8 +35,17 @@ class UsersController < ApplicationController
   end
 
   private
+
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in.'
+        redirect_to login_url
+      end
+    end
+    
 end
