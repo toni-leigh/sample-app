3614a39e0a183b4acc2fde2ded17b8f5f02164c4
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-02-10 14:33:46 +0000

    add some site layout tests focusing on expected links when a user is signed in

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -10,5 +14,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+
+    log_in_as(@user)
+    get users_path
+
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", edit_user_path(@user)
+    
+    assert_select "a[href=?]", signup_path, false
+    assert_select "a[href=?]", login_path, false
+
   end
 end
