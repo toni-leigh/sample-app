1933579f369227b10ca87a76a2b263626daa1f51
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-02-12 09:15:35 +0000

    add functions for the action of following, unfollowing and to find if a user is or isn't following another user

--- app/models/user.rb
+++ app/models/user.rb
@@ -90,6 +90,22 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
 
+  # Follow a user
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollow a user
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+
   private
 
     # Converts email to all lower-case
